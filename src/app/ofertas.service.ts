0f41e46814cf91af6cb99845739e8ad1ddee5842
unknown <jorgesoprani@gmail.com>
2019-09-23 22:51:17 -0300

    Parei na aula 129

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,65 +1,31 @@
+import { Http } from '@angular/http';
 import { Oferta } from './shared/oferta.model';
-export class OfertasService {   
-    public ofertas: Array<Oferta> = [
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/1/img1.jpg"},
-                {url: "/assets/ofertas/1/img2.jpg"},
-                {url: "/assets/ofertas/1/img3.jpg"},
-                {url: "/assets/ofertas/1/img4.jpg"}
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/2/img1.jpg"},
-                {url: "/assets/ofertas/2/img2.jpg"},
-                {url: "/assets/ofertas/2/img3.jpg"},
-                {url: "/assets/ofertas/2/img4.jpg"}
-            ]
-        
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Estância das águas",
-            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/3/img1.jpg"},
-                {url: "/assets/ofertas/3/img2.jpg"},
-                {url: "/assets/ofertas/3/img3.jpg"},
-                {url: "/assets/ofertas/3/img4.jpg"},
-                {url: "/assets/ofertas/3/img5.jpg"},
-                {url: "/assets/ofertas/3/img6.jpg"}
-            ]
-        }
-    ];
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+import { URL_API } from './app.api';
 
-    public getOfertas() : Promise<Array<Oferta>> {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                let teste: boolean = true;
-                if(teste)
-                    resolve(this.ofertas); 
-                else
-                    reject({ error: 404, message: 'blaldjalksdjalksdj' });
-            }, 500);
-        });
+@Injectable()
+export class OfertasService {   
+    private service: string = 'ofertas';
+    private serviceUrl: string = `${URL_API}/${this.service}`;
+
+    constructor(private http: Http) {}
+    
+    public getOfertas() : Promise<Oferta[]> {
+        return this.http.get(`${this.serviceUrl}?destaque=true`)
+        .toPromise()
+        .then((resposta: any) => resposta.json());
+    }
+
+    public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
+        return this.http.get(`${this.serviceUrl}?categoria=${categoria}`)
+        .toPromise()
+        .then((resposta: any) => resposta.json());
+    }
+
+    public getOfertasPorId(id: number) : Promise<Oferta> {
+        return this.http.get(`${this.serviceUrl}?id=${id}`)
+        .toPromise()
+        .then((resposta: any) => resposta.json()[0]);
     }
 }
