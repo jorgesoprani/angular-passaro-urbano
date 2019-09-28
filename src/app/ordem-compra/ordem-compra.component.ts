2765f947f91f898d0965345a87ebe17deb87d8c4
Jorge Soprani <jorgesoprani@gmail.com>
2019-09-28 14:29:20 -0300

    sdasdasd

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pedido } from '../shared/pedido.model';
+import { OrdemCompraService } from '../ordem-compra.service';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ordem-compra.component.css']
 })
 export class OrdemCompraComponent implements OnInit {
-
-  constructor() { }
+  public model: Pedido = new Pedido('', '', '', '');
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
   }
 
+  confirmarCompra() : void {
+    this.ordemCompraService.confirmarCompra(this.model);
+  }
 }
